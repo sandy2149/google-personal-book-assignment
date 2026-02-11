9903f4e04c0c0a5a4026f1bbf3e2f14a72bb0827
DESKTOP-UAS6EEJ\Sandeep Chail <sandy4docs@gmail.com>
2026-02-12 00:31:44 +0530

    Initial Spring Boot project setup with Maven, JPA, H2 and Lombok

---
+++ src/main/java/com/example/google_personal_book_assignment/GooglePersonalBookAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.google_personal_book_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglePersonalBookAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglePersonalBookAssignmentApplication.class, args);
+	}
+
+}
