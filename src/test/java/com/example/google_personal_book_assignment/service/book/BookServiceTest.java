1da574404900876b325c4c5322aaf2b11f4d8e17
DESKTOP-UAS6EEJ\Sandeep Chail <sandy4docs@gmail.com>
2026-02-12 01:45:07 +0530

    adding tests for service layer

---
+++ src/test/java/com/example/google_personal_book_assignment/service/book/BookServiceTest.java
@@ -0,0 +1,91 @@
+package com.example.google_personal_book_assignment.service.book;
+
+import com.example.google_personal_book_assignment.dto.GoogleBookResponse;
+import com.example.google_personal_book_assignment.entity.Book;
+import com.example.google_personal_book_assignment.repository.BookRepository;
+import com.example.google_personal_book_assignment.service.google.GoogleBookService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+class BookServiceTest {
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @Mock
+    private GoogleBookService googleBookService;
+
+    @InjectMocks
+    private BookService bookService;
+
+    private GoogleBookResponse googleBookResponse;
+
+    @BeforeEach
+    void setup() {
+        googleBookResponse = new GoogleBookResponse();
+        googleBookResponse.setId("J1001");
+
+        GoogleBookResponse.VolumeInfo volumeInfo = new GoogleBookResponse.VolumeInfo();
+        volumeInfo.setTitle("Spring Boot Book");
+        volumeInfo.setAuthors(List.of("Kathy Siera"));
+        volumeInfo.setPageCount(1010);
+
+        googleBookResponse.setVolumeInfo(volumeInfo);
+    }
+
+    @Test
+    void addBookSuccessfully() {
+        Mockito.when(bookRepository.existsByGoogleId("J1001")).thenReturn(false);
+        Mockito.when(googleBookService.getBookById("J1001")).thenReturn(googleBookResponse);
+        Mockito.when(bookRepository.save(Mockito.any(Book.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        Book book1 = bookService.addBookFromGoogle("J1001");
+
+        Assertions.assertEquals("J1001", book1.getGoogleId());
+        Assertions.assertEquals("Spring Boot Book", book1.getTitle());
+        Assertions.assertEquals("Kathy Siera", book1.getAuthor());
+        Assertions.assertEquals(1010, book1.getPageCount());
+    }
+
+    @Test
+    void returnsAuthorNotFoundIfAuthorsListEmpty() {
+        googleBookResponse.getVolumeInfo().setAuthors(List.of());
+        Mockito.when(bookRepository.existsByGoogleId("J1001")).thenReturn(false);
+        Mockito.when(googleBookService.getBookById("J1001")).thenReturn(googleBookResponse);
+        Mockito.when(bookRepository.save(Mockito.any(Book.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        Book result = bookService.addBookFromGoogle("J1001");
+        Assertions.assertEquals("Author not found", result.getAuthor());
+    }
+
+    @Test
+    void returnsBookList() {
+        Book book1 = Book.builder().title("Java").build();
+        Book book2 = Book.builder().title("React").build();
+        Book book3 = Book.builder().title("SQL").build();
+        Mockito.when(bookRepository.findAll()).thenReturn(List.of(book1, book2, book3));
+        List<Book> books = bookService.getAllBooks();
+        Assertions.assertEquals(3, books.size());
+    }
+
+    //Negative Cases
+    @Test
+    void throwsExceptionIfBookNotFoundInGoogle() {
+        Mockito.when(bookRepository.existsByGoogleId("J1001")).thenReturn(false);
+        Mockito.when(googleBookService.getBookById("J1001")).thenReturn(null);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> bookService.addBookFromGoogle("J1001"));
+    }
+
+    @Test
+    void throwsExceptionIfDuplicateBook() {
+        Mockito.when(bookRepository.existsByGoogleId("J1001")).thenReturn(true);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> bookService.addBookFromGoogle("J1001"));
+    }
+}
---
+++ src/test/java/com/example/google_personal_book_assignment/service/google/GoogleBookServiceTest.java
@@ -0,0 +1,62 @@
+package com.example.google_personal_book_assignment.service.google;
+
+import com.example.google_personal_book_assignment.dto.GoogleBookResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.RestTemplate;
+
+@ExtendWith(MockitoExtension.class)
+class GoogleBookServiceTest {
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    private GoogleBookService googleBookService;
+
+    private final String url = "https://www.googleapis.com/books/v1";
+
+    @BeforeEach
+    void setup() {
+        ReflectionTestUtils.setField(googleBookService,"baseUrl", url);
+    }
+
+    @Test
+    void getBookSuccessfully() {
+        GoogleBookResponse googleBookResponse = new GoogleBookResponse();
+        googleBookResponse.setId("J1001");
+
+        String completeUrl = url + "/volumes/J1001";
+        Mockito.when(restTemplate.getForObject(completeUrl, GoogleBookResponse.class))
+                .thenReturn(googleBookResponse);
+        GoogleBookResponse googleBookResponseResult = googleBookService.getBookById("J1001");
+
+        Assertions.assertNotNull(googleBookResponseResult);
+        Assertions.assertEquals("J1001", googleBookResponseResult.getId());
+    }
+
+    // Negative Cases
+
+    @Test
+    void gettingNullWhenApiReturningNull() {
+        String completeUrl = url + "/volumes/J1001";
+        Mockito.when(restTemplate.getForObject(completeUrl, GoogleBookResponse.class)).thenReturn(null);
+        GoogleBookResponse googleBookResponse = googleBookService.getBookById("J1001");
+        Assertions.assertNull(googleBookResponse);
+    }
+
+    @Test
+    void throwsExceptionIfApiFails() {
+        String completeUrl = url + "/volumes/J1001";
+        Mockito.when(restTemplate.getForObject(completeUrl, GoogleBookResponse.class)).thenThrow(new RuntimeException("API fails"));
+        Assertions.assertThrows(RuntimeException.class, () -> googleBookService.getBookById("J1001")
+        );
+    }
+}
