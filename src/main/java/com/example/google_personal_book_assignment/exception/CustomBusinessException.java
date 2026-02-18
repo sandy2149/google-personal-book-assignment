e20865b6057ddd0a8dc3e937c87495ef88316855
DESKTOP-UAS6EEJ\Sandeep Chail <sandy4docs@gmail.com>
2026-02-18 20:37:45 +0530

    Added CustomBusinessException and error constants files

---
+++ src/main/java/com/example/google_personal_book_assignment/exception/CustomBusinessException.java
@@ -0,0 +1,8 @@
+package com.example.google_personal_book_assignment.exception;
+
+public class CustomBusinessException extends RuntimeException {
+    public CustomBusinessException(String message) {
+        super(message);
+    }
+}
+
---
+++ src/main/java/com/example/google_personal_book_assignment/exception/ErrorConstants.java
@@ -0,0 +1,11 @@
+package com.example.google_personal_book_assignment.exception;
+
+
+public final class ErrorConstants {
+
+    private ErrorConstants() {}
+
+    public static final String BOOK_ALREADY_EXISTS = "Book already exists";
+    public static final String BOOK_NOT_FOUND = "Book id not found";
+}
+
